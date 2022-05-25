be39da30bc23a5e778df52b1d4d8dec98f5b0e76
noisersup <patryk@kwiatek.xyz>
2022-05-25 21:16:22 +0200

    blurlock rm from /tmp

--- blurlock/default.nix
+++ blurlock/default.nix
@@ -7,4 +7,5 @@ pkgs.writeShellScriptBin "blurlock" ''
    
   ${pkgs.ffmpeg}/bin/ffmpeg -hide_banner -loglevel error -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG
   ${pkgs.i3lock}/bin/i3lock -eui $TMPBG
+  rm /tmp/screen.png
 ''
