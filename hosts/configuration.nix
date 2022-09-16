059338fd1aa1ee65371f7c59cbf6269c71f3c94d
noisersup <patryk@kwiatek.xyz>
2022-09-16 23:20:52 +0200

    changes

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -13,6 +13,15 @@ in
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    settings = {
+      substituters = [
+        "http://192.168.0.125:5000" 
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "192.168.0.125:rnznZvCV+uLl9c1ZPmjYah408khUqx4iTwg/qVfBwlo="
+      ];
+    };
   };
 
   nixpkgs.config = {
@@ -45,6 +54,7 @@ in
     192.168.0.125 lidarr.giratina
     192.168.0.125 jackett.giratina
     192.168.0.125 nextcloud.giratina
+    192.168.0.125 cock.giratina
   '';
 
   time.timeZone = "Europe/Warsaw";
@@ -102,7 +112,7 @@ in
   environment.systemPackages = with pkgs; [
     (import ../nvim/nvim.nix { pkgs = pkgs; })
     git
-    go_1_18
+    go_1_19
     gopls
     fd
     tree-sitter
@@ -199,6 +209,46 @@ hardware.opengl = {
   #  user = "user";
   #  openFirewall = true;
   #};
+
+  networking.firewall = {
+    allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
+  };
+  # Enable WireGuard
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the client's end of the tunnel interface.
+      ips = [ "10.100.0.2/24" ];
+      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "/root/wg-private";
+
+      peers = [
+        # For a client configuration, one peer entry for the server will suffice.
+
+        {
+          # Public key of the server (not a file path).
+          publicKey = "ZT8UX1yYyEj2dUeigSriTxILQeqVDgnI0Tp+w88Q8SY=";
+
+          # Forward all the traffic via VPN.
+          #allowedIPs = [ "0.0.0.0/0" ];
+          # Or forward only particular subnets
+          allowedIPs = [ "10.100.0.1" ];
+
+          # Set this to the server IP and port.
+          endpoint = "192.168.0.125:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+
+          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
 
   system.stateVersion = "21.11";
 }
